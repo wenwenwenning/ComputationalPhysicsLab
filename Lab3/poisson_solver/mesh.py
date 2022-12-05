7fb43ca49a63253240e1a0a79358d2df4cad18c2
Kuo-Chuan Pan <kuochuan.pan@gapp.nthu.edu.tw>
2022-12-05 11:47:58 +0800

    Added Lab3 templates

---
+++ Lab3/poisson_solver/mesh.py
@@ -0,0 +1,16 @@
+"""
+This file define classes for generating 2D meshes.
+
+"""
+import numpy as np
+from numba import jit, int32, float64
+from numba.experimental import jitclass
+
+class Mesh2D:
+    def __init__(self):
+        return
+
+
+if __name__=='__main__':
+
+    print("Testing ... nx=10, ny=10, buff =1")
